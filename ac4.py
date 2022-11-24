b7f044afb89941bbafa6e29eff2254357463fd68
Lucy Ivatt <luceapuce@gmail.com>
2022-11-24 16:52:28 +0000

    removed lecture test and ac3 test, began neatening for submission

--- ac4.py
+++ ac4.py
@@ -1,6 +1,6 @@
 import itertools
 import logging
-from typing import Callable, Dict, Set, Any
+from typing import Callable, Dict, Set, Any, Tuple
 
 logging.basicConfig(level=logging.INFO, format='%(message)s')
 str_line = "-" * 30
@@ -25,6 +25,10 @@ class Arc:
         return hash((self.xi, self.xj))
 
 
+class DomainWipeout(Exception):
+    pass
+
+
 # Types for Constraint Functions, Constraint Store and Domains
 # xi, di, xj, dj parameter for ConstraintFunction Callables
 ConstraintFunction = Callable[[int, Any, int, Any], bool]
@@ -44,6 +48,7 @@ class AC4:
         """
 
         logging.debug(str_line + "\nAC4: Beginning setup\n" + str_line)
+
         self.input_domains = input_domains
         self.constraints = constraints
 
@@ -84,30 +89,38 @@ class AC4:
         # If the pairs satisfy the constraint, increment the support counter for di and add it to the set S for
         # xj, dj to show the dj provides support for this assignment.
 
-        for arc, constraint in self.constraints.items():
-            for di in self.input_domains[arc.xi]:
-                for dj in self.input_domains[arc.xj]:
-                    if self.M[(arc.xi, di)] != 1 or self.M[(arc.xj, dj)] != 1:
-                        if self.checkConstraint(arc.xi, di, arc.xj, dj, constraint) == True:
-                            self.S[arc.xj, dj].add((arc.xi, di))
-                            self.Counter[(arc, di)] += 1
+        try:
+            for arc, constraint in self.constraints.items():
+                for di in self.input_domains[arc.xi]:
+                    for dj in self.input_domains[arc.xj]:
+                        if self.M[(arc.xi, di)] != 1 or self.M[(arc.xj, dj)] != 1:
+                            if self.checkConstraint(arc.xi, di, arc.xj, dj, constraint) == True:
+                                self.S[arc.xj, dj].add((arc.xi, di))
+                                self.Counter[(arc, di)] += 1
 
-                            logging.debug(
-                                f"Initialise: Incremented counter {arc}, {di} = {self.Counter[(arc, di)]}")
+                                logging.debug(
+                                    f"Initialise: Incremented counter {arc}, {di} = {self.Counter[(arc, di)]}")
 
-                # If no support for di and it hasn't already been removed, update M to show deletion and add deletion
-                # to L to propagate later.
-                if self.Counter[(arc, di)] == 0 and self.M[(arc.xi, di)] != 1:
-                    self.M[(arc.xi, di)] = 1
-                    self.L.append((arc.xi, di))
-                    logging.debug(f"Initialise: deleting x{arc.xi}, {di}")
+                    # If no support for di and it hasn't already been removed, update M to show deletion and add deletion
+                    # to L to propagate later.
+                    if self.Counter[(arc, di)] == 0 and self.M[(arc.xi, di)] != 1:
+                        self.M[(arc.xi, di)] = 1
+                        self.L.append((arc.xi, di))
+                        logging.debug(f"Initialise: deleting x{arc.xi}, {di}")
 
-                    self.current_domains = {xi: set()
-                                            for xi in self.input_domains.keys()}
-                    for key, switch in self.M.items():
-                        xi, di = key
-                        if switch == 0:
-                            self.current_domains[xi].add(di)
+                        # Updates the current_domains dictionary when a value is deleted, by reading the switches in M
+                        self.current_domains = {xi: set()
+                                                for xi in self.input_domains.keys()}
+                        for key, switch in self.M.items():
+                            xi, di = key
+                            if switch == 0:
+                                self.current_domains[xi].add(di)
+                        for xi, Di in self.current_domains.items():
+                            if len(Di) == 0:
+                                raise DomainWipeout
+        except DomainWipeout:
+            logging.info(
+                f"Initialise: Domain wipeout detected, stopping AC4 algorithm")
 
         logging.debug(
             f"Initialise: List of deletions to propagate - {''.join([f'(x{xi}, {di}) ' for xi, di in self.L])}")
@@ -120,32 +133,42 @@ class AC4:
          and decrementing their counters. If a counter reaches 0 and hasn't already been deleted then delete it
          and add to L. Continues until L is empty.
         """
-        logging.debug(str_line + "\nPropagate: Beginning Phase\n" + str_line)
-        while len(self.L) > 0:
-            # Remove an element from L (arbitrarily decided to pick the first element but order is unimportant)
-            xj, dj = self.L[0]
-            self.L.pop(0)
+        try:
+            logging.debug(
+                str_line + "\nPropagate: Beginning Phase\n" + str_line)
+            while len(self.L) > 0:
+                # Remove an element from L (arbitrarily decided to pick the first element but order is unimportant)
+                xj, dj = self.L[0]
+                self.L.pop(0)
 
-            # Decrements the counters of assignments support by the deleted value
-            for xi, di in self.S[(xj, dj)]:
-                arc = Arc((xi, xj))
-                self.Counter[(arc, di)] -= 1
-                logging.debug(
-                    f"Propagate: updated counter {arc}, {di} = {self.Counter[(arc, di)]}")
-
-                # If a value has no supports and isn't already deleted, delete it and add to L
-                if self.Counter[(arc, di)] == 0 and self.M[(xi, di)] != 1:
-                    self.M[(xi, di)] = 1
-                    self.L.append((xi, di))
+                # Decrements the counters of assignments support by the deleted value
+                for xi, di in self.S[(xj, dj)]:
+                    arc = Arc((xi, xj))
+                    self.Counter[(arc, di)] -= 1
                     logging.debug(
-                        f"Propagate: No supports found - deleting value x{xi}, {di}")
+                        f"Propagate: updated counter {arc}, {di} = {self.Counter[(arc, di)]}")
 
-                    self.current_domains = {xi: set()
-                                            for xi in self.input_domains.keys()}
-                    for key, switch in self.M.items():
-                        xi, di = key
-                        if switch == 0:
-                            self.current_domains[xi].add(di)
+                    # If a value has no supports and isn't already deleted, delete it and add to L
+                    if self.Counter[(arc, di)] == 0 and self.M[(xi, di)] != 1:
+                        self.M[(xi, di)] = 1
+                        self.L.append((xi, di))
+                        logging.debug(
+                            f"Propagate: No supports found - deleting value x{xi}, {di}")
+
+                        self.current_domains = {xi: set()
+                                                for xi in self.input_domains.keys()}
+
+                        for key, switch in self.M.items():
+                            xi, di = key
+                            if switch == 0:
+                                self.current_domains[xi].add(di)
+
+                        for xi, Di in self.current_domains.items():
+                            if len(Di) == 0:
+                                raise DomainWipeout
+        except DomainWipeout:
+            logging.info(
+                f"Propagate: Domain wipeout detected, stopping AC4 algorithm")
 
         # Prints final domains
         logging.info(f"Propagate: Final Domains")
@@ -203,104 +226,38 @@ def no_constraint(xi, di, xj, dj) -> bool:
     """
     return True
 
-
-def less_than_constraint(xi, di, xj, dj) -> bool:
-    return di < dj
-
-
-def greater_than_constraint(xi, di, xj, dj) -> bool:
-    return di > dj
-
 # ----------------------------------------------------------------------------
 
 
-# def lecture_example_test():
-#     domains = {0: {2, 10, 16}, 1: {9, 12, 21},
-#                2: {9, 10, 11}, 3: {2, 5, 10, 11}}
+def nqueens_ac4_test(num_queens, test_case):
+    """Runs the AC4 algorithm on the nqueens problem by generating the required domains & constraints and calling 
+    the AC4 class. Also restricts domains of decision variables defined by the test case to their single assigned
+    value before running. 
 
-#     constraints = {Arc((0, 1)): less_than_constraint,
-#                    Arc((0, 2)): less_than_constraint,
-#                    Arc((0, 3)): less_than_constraint,
-#                    Arc((1, 0)): greater_than_constraint,
-#                    Arc((1, 2)): less_than_constraint,
-#                    Arc((1, 3)): less_than_constraint,
-#                    Arc((2, 0)): greater_than_constraint,
-#                    Arc((2, 1)): greater_than_constraint,
-#                    Arc((2, 3)): greater_than_constraint,
-#                    Arc((3, 0)): greater_than_constraint,
-#                    Arc((3, 1)): greater_than_constraint,
-#                    Arc((3, 2)): less_than_constraint}
-#     ac4graph = AC4()
-#     ac4graph.ac4(domains, constraints)
+    Args:
+        num_queens: width of the chess board (i.e. number of queens to place)
+        test_case: List of (xi, di) assignment pairs
+    """
+    # Creates domains/constraints - permutations used as no unary constraints applied
+    domains = {n: set(range(num_queens)) for n in range(num_queens)}
+    constraints = {Arc(n): nqueens_constraint for n in itertools.permutations(
+        range(num_queens), 2)}
 
-
-NUM_QUEENS = 6
-
-def nqueens_ac4_test(assignments):
-    domains = {n: set(range(NUM_QUEENS)) for n in range(NUM_QUEENS)}
-    constraints = {
-        Arc(n): nqueens_constraint for n in itertools.permutations(range(NUM_QUEENS), 2)}
-
-    for xi, di in assignments:
+    # Restricts decision variables from the test case to be only the assigned value
+    for xi, di in test_case:
         domains[xi] = set([di])
 
-    nqueenac4 = AC4()
-    nqueenac4.ac4(domains, constraints)
-
-def ac3():
-    Q = [n for n in itertools.permutations(range(NUM_QUEENS), 2)]
-
-    while len(Q) != 0:
-        i, j = Q.pop(0)
-        changed = reviseAC3(i, ac3_domains[i], j, ac3_domains[j])
-        if changed:
-            Q.extend([(x, y) for x, y in itertools.permutations(range(NUM_QUEENS), 2) if x == i or x == j])
+    # Runs the algorithm
+    nqueensAC4 = AC4()
+    nqueensAC4.ac4(domains, constraints)
 
 
-def reviseAC3(i, Di, j, Dj):
-    change = False
-    to_remove = []
-    for di in Di:
-        supported = False
-        for dj in Dj:
-            if i == 4:
-                print(f"Checking value x{i}={di} and d{j}={dj}")
-            if nqueens_constraint(i, di, j, dj):
-                supported = True
-                if i == 4:
-                    print("Supports this")
-            else:
-                if i == 4:
-                    print("doesnt support this")
-        if supported == False:
-            print(f"{di} from x{i}")
-            to_remove.append(di)
-            change = True
-        else:
-            print("------")
-        
-    
-    for i in to_remove:
-        Di.remove(i)
-    if len(ac3_domains[i]) == 0:
-        print ("empty domain failure")
-    return change
-
-test_assignments = [(0, 1), (1, 3), (2, 5)]
+# Test cases from assessment rubric - pairs of (xi, di) assignments
+test_cases = [[(0, 0), (1, 2)],
+              [(0, 0), (1, 3)],
+              [(0, 1), (1, 3), (2, 5)]]
 
 
-ac3_domains = {n: set(range(NUM_QUEENS)) for n in range(NUM_QUEENS)}
-ac3_constraints = {Arc(n): nqueens_constraint for n in itertools.permutations(range(NUM_QUEENS), 2)}
-
-for x in ac3_constraints.keys():
-    print(x)
-
-for i, di in test_assignments:
-    ac3_domains[i] = set([di])
-ac3()
-for key, val in ac3_domains.items():
-    print(key, val)
-
-# runs the three test cases as defined in the assessment brief
-
-nqueens_ac4_test(test_assignments)
+# Runs the three test cases as defined in the assessment brief
+for case in test_cases:
+    nqueens_ac4_test(num_queens=6, test_case=case)
